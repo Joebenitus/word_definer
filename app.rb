c4ed9b9ef6952d408718dfd7388779b6587c6d62
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-02 11:00:07 -0700

    add basic routing and UI with the ability to add words to the list

--- app.rb
+++ app.rb
@@ -1,6 +1,28 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/album')
-require('./lib/song')
+require('./lib/word')
+require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:word_new )
+end
+
+post('/words') do
+  word = params[:word]
+  word_obj = Word.new({:name => word, :id => nil})
+  word_obj.save()
+  @words = Word.all
+  erb(:words)
+end
