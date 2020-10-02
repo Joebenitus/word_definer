3cee29eb81d2c1e97d0ee90a2cdd0b6ca251dcc4
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-02 08:46:47 -0700

    add passing test for self.all method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,2 +1,10 @@
 require 'rspec'
 require 'word'
+
+describe('#Word') do
+  describe('.all') do
+    it('returns an empty array if there are no words') do
+      expect(Word.all).to(eq([]))
+    end
+  end
+end
